c5a557db55199ae3be3767ff68b8f8cd3c93e179
Greg Jordan-Detamore <greg@gregjd.com>
2018-10-18 10:51:17 -0400

    Fix highlighting in header
    so that it will work on regular GitHub Pages URLs / if there's a baseurl

--- assets/js/fix-usa-current.js
+++ assets/js/fix-usa-current.js
@@ -1,8 +1,17 @@
+---
+---
 // Workaround to fix the issue of usa-current not being applied correctly
 var navs = $('nav').not('#toc, .footer-mobile, .footer-desktop');
 var links = $('ul li a', navs);
 links.removeClass('usa-current');
-var where = window.location.pathname.split('/')[1];
+var baseurl = '{{ site.baseurl }}';
+baseurl = baseurl.replace('/', '');
+var pathname_split = window.location.pathname.split('/');
+if (pathname_split[1] === baseurl) {
+  var where = pathname_split[1] + '/' + pathname_split[2];
+} else {
+  var where = pathname_split[1];
+}
 var where_slash = '/' + where + '/';
 var url_matches = links.filter( function (i) {
   return this.pathname === where_slash;
